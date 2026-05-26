817d8ef58c262a0be4e9306e62fef3ae1331a7f4
sertacekici <sertacekici@gmail.com>
2026-05-26 21:16:23 +0300

    db: support FIREBASE_SERVICE_ACCOUNT env var for Heroku

--- db.js
+++ db.js
@@ -1,6 +1,8 @@
 const admin = require("firebase-admin");
 const { initializeApp } = require('firebase-admin/app');
-const serviceAccount = require("./fb/to/adel.json");
+const serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT
+  ? JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
+  : require("./fb/to/adel.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
